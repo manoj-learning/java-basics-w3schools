047d767a23da71de02948dc3efe07b89d1b28220
Manoj Kumar <dharmakumar338@gmail.com>
2021-11-15 18:56:24 +0530

    arrays final

--- src/com/manoj/learning/java/basics/arrays/Main.java
+++ src/com/manoj/learning/java/basics/arrays/Main.java
@@ -34,6 +34,13 @@ public class Main {
 		int[][] myNumbers = { { 1, 2, 3, 4 }, { 5, 6, 7 } };
 		int x = myNumbers[1][2];
 		System.out.println(x);
+		
+		//loop through 2D array
+	    for (int i = 0; i < myNumbers.length; ++i) {
+	      for(int j = 0; j < myNumbers[i].length; ++j) {
+	        System.out.println(myNumbers[i][j]);
+	      }
+	    }
 	}
 
 }
